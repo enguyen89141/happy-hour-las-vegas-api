068bf7ce416d321d0aacb6cfe1059515fe684575
Eric Nguyen <enguyen89141@gmail.com>
2019-11-06 18:20:50 -0800

    tested initial hello world endpoin

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const supertest = require('supertest')
 
-const app = require('../app')
+const app = require('../src/app')
 
 describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
